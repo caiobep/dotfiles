9d20e78cd369c325a10e7203a588b8b4676d817f
Caio Amaral Corrêa <caiobep@me.com>
2018-09-17 18:42:26 -0300

    Make ZSH use TMUX on start

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,7 +1,17 @@
+if command -v tmux > /dev/null; then
+     [ -z $TMUX ] && exec tmux
+else
+    echo "Tmux not installed."
+fi
+
+# Update Settings
+#(cd ~/.dotfiles && git pull && git submodule update --init --recursive)
+
 foreach piece (
-	aliases.zsh
-	exports.zsh
-	functions.zsh
-	plugins.zsh
+    aliases.zsh
+    exports.zsh
+    functions.zsh
+    plugins.zsh
     mappings.zsh
 ) { source ~/.dotfiles/zsh/config/$piece }
+
