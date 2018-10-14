a705289ed235ac0bff6c9107297652fab0c882d1
Caio Amaral Corrêa <caiobep@me.com>
2018-10-14 18:10:30 -0300

    Updated vimplug installation

--- install.sh
+++ install.sh
@@ -71,7 +71,7 @@ backup_dotfiles() {
 }
 
 install_vim_plug() {
-    curl -fLo ~/.dotfiles/autoload/plug.vim --create-dirs \
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
