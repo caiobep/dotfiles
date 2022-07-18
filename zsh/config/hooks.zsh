f2aa164274c016c39ca1100d40dd882e3d713e61
Caio Amaral Corrêa <caiobep@me.com>
2022-07-18 06:17:40 -0300

    feat: nvm autoswitch based on package.json engine property

--- zsh/config/hooks.zsh
+++ zsh/config/hooks.zsh
@@ -3,7 +3,16 @@
 autoload -U add-zsh-hook
 load-nvmrc() {
   local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc)"
+  local nvmrc_path="$(nvm_find_nvmrc)" 
+
+  if [ -z "$nvmrc_path" ] && [ -f package.json ]; then
+    local nvm_version_in_package_json="$(/opt/homebrew/bin/gcat package.json | /opt/homebrew/bin/jq '.engines.node' | sed 's/[\^\~\"]//gi')"
+
+    if [ -n "$nvm_version_in_package_json" ]; then
+      nvm install $nvm_version_in_package_json
+      nvm use $nvm_version_in_package_json
+    fi
+  fi
 
   if [ -n "$nvmrc_path" ]; then
     local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
@@ -14,7 +23,6 @@ load-nvmrc() {
       nvm use
     fi
   elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
     nvm use default
   fi
 }
