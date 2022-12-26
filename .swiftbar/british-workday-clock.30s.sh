d02abe241a738214e9d6a20fa2b2c30ab0577018
Caio Amaral Corrêa <caiobep@me.com>
2022-12-26 07:58:57 -0300

    feat: add swiftbar script

--- .swiftbar/british-workday-clock.30s.sh
+++ .swiftbar/british-workday-clock.30s.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
-BRITISH_TIME="$(TZ=":Europe/London" date +"%H:%M")"
+BRITISH_HOUR="$(TZ=":Europe/London" date +"%H")"
 
-if [[ $(date +%u) -lt 6 ]] && [[ "$BRITISH_TIME" > 08:30 ]] && [[ "$BRITISH_TIME" < 18:00 ]]; then
+if [[ $(date +%u) -lt 6 ]] && [[ "$BRITISH_HOUR" > 07 ]] && [[ "$BRITISH_HOUR" < 18 ]]; then
+    BRITISH_TIME="$(TZ=":Europe/London" date +"%H:%M")"
     echo "🇬🇧  $BRITISH_TIME"
 fi
 
