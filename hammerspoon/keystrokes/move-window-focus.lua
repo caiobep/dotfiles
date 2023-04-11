d27fe6ffc437f56f3ad0e41c0587cb6661420097
Caio Amaral Corrêa <caiobep@me.com>
2023-04-11 09:09:43 -0300

    feat: optimize move-window-focus

--- hammerspoon/keystrokes/move-window-focus.lua
+++ hammerspoon/keystrokes/move-window-focus.lua
@@ -1,55 +1,38 @@
+function findNeighbor(direction)
+  local win = hs.window.focusedWindow()
+  if not win then return nil end
+
+  local winFrame = win:frame()
+  local neighbor = nil
+  local minDistance = math.huge
+
+  hs.fnutils.each(hs.window.visibleWindows(), function(candidate)
+    if candidate:isStandard() and candidate:application() ~= win:application() then
+      local candidateFrame = candidate:frame()
+      local validCandidate = false
+      local distance = 0
+
+      if direction == "left" and candidateFrame.x + candidateFrame.w * 0.5 < winFrame.x then
+        validCandidate = candidateFrame.y + candidateFrame.h > winFrame.y and candidateFrame.y < winFrame.y + winFrame.h
+        distance = winFrame.x - (candidateFrame.x + candidateFrame.w)
+      elseif direction == "right" and candidateFrame.x > winFrame.x + winFrame.w * 0.5 then
+        validCandidate = candidateFrame.y + candidateFrame.h > winFrame.y and candidateFrame.y < winFrame.y + winFrame.h
+        distance = candidateFrame.x - (winFrame.x + winFrame.w)
+      end
+
+      if validCandidate and distance < minDistance then
+        minDistance = distance
+        neighbor = candidate
+      end
+    end
+  end)
+
+  return neighbor
+end
+
 function moveFocusToWindow(direction)
-  if direction == "left" then
-    local win = hs.window.focusedWindow()
-    if not win then return end
-
-    local winFrame = win:frame()
-    local leftNeighbor = nil
-    local minDistance = math.huge
-
-    hs.fnutils.each(hs.window.visibleWindows(), function(candidate)
-      if candidate:isStandard() and candidate:application() ~= win:application() then
-        local candidateFrame = candidate:frame()
-        if candidateFrame.x + candidateFrame.w * 0.5 < winFrame.x and candidateFrame.y + candidateFrame.h > winFrame.y
-            and candidateFrame.y < winFrame.y + winFrame.h then
-          local distance = winFrame.x - (candidateFrame.x + candidateFrame.w)
-          if distance < minDistance then
-            minDistance = distance
-            leftNeighbor = candidate
-          end
-        end
-      end
-    end)
-
-    if leftNeighbor then
-      leftNeighbor:focus()
-    end
-  end
-
-  if direction == "right" then
-    local win = hs.window.focusedWindow()
-    if not win then return end
-
-    local winFrame = win:frame()
-    local rightNeighbor = nil
-    local minDistance = math.huge
-
-    hs.fnutils.each(hs.window.visibleWindows(), function(candidate)
-      if candidate:isStandard() and candidate:application() ~= win:application() then
-        local candidateFrame = candidate:frame()
-        if candidateFrame.x > winFrame.x + winFrame.w * 0.5 and candidateFrame.y + candidateFrame.h > winFrame.y and
-            candidateFrame.y < winFrame.y + winFrame.h then
-          local distance = candidateFrame.x - (winFrame.x + winFrame.w)
-          if distance < minDistance then
-            minDistance = distance
-            rightNeighbor = candidate
-          end
-        end
-      end
-    end)
-
-    if rightNeighbor then
-      rightNeighbor:focus()
-    end
+  local neighbor = findNeighbor(direction)
+  if neighbor then
+    neighbor:focus()
   end
 end
